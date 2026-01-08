de4005ebca4a6961cccbb48b99dbd7b48b8311c4
fazal-e-haq <fazalehaq37405@GMAIL.COM>
2026-01-09 01:32:37 +0500

    add folders

---
+++ lib/presentation/Pages/computer/computer_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ComputerHomePage extends StatelessWidget {
+  const ComputerHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(decoration: BoxDecoration()));
+  }
+}
---
+++ lib/presentation/Pages/tab/tab_home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class TabHomePage extends StatelessWidget {
+  const TabHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('tab');
+  }
+}
